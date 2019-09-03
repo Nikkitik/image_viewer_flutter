e4b37b2a78b3a6a98d2228aa78dfdbd2f63daf40
nikita.romantsov <nikkitik21@gmail.com>
2019-09-03 12:35:30 +0300

    добавил описание размеров

--- lib/widgets/picture_show.dart
+++ lib/widgets/picture_show.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:image_viewer_flutter/models/photos.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
+/*
+Size Suffixes
+The letter suffixes are as follows:
+
+  s	small square 75x75
+  q	large square 150x150
+  t	thumbnail, 100 on longest side
+  m	small, 240 on longest side
+  n	small, 320 on longest side
+  -	medium, 500 on longest side
+  z	medium 640, 640 on longest side
+  c	medium 800, 800 on longest side†
+  b	large, 1024 on longest side*
+  h	large 1600, 1600 on longest side†
+  k	large 2048, 2048 on longest side†
+  o	original image, either a jpg, gif or png, depending on source format
+ */
+
 class PictureShow extends StatelessWidget {
   final Photo photo;
 
